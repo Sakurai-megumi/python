86d7c8078d698c99f992c08a49c3eb1e5228e87d
megumi-sakurai <sakuraimegumi>
2022-04-07 20:34:28 +0900

    練習

--- 練習/001.py
+++ 練習/001.py
@@ -1,1 +1,27 @@
-print(100+1)
+# print(100+1)
+
+
+# x = 123456789
+# print(x)
+# print(x+1)
+
+# s = "黄昏の獅子から暁の乙女へ"
+# print(s[11])
+# print(s +"-> "+s[12-1])
+
+from dataclasses import replace
+from hashlib import sha3_224
+
+
+# s = "黄昏の獅子から暁の乙女へ"
+# print(s[3:5])
+# print(s[:5])
+# print(s[7:])
+# print(s.replace("乙女","處女"))
+
+s1 = "あなたは"
+s2 = "番目のお客様です"
+i = 11235
+
+print(s1+str(i)+s2)
+#print(s1+i+s2)  #エラー
