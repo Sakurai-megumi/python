a1f46ad001d96bb0c7d0857907860ef1f648fb01
megumi-sakurai <sakuraimegumi>
2022-04-08 21:28:52 +0900

    設定

--- 練習/kanzen-nyumon.py
+++ 練習/kanzen-nyumon.py
@@ -1,1 +1,29 @@
-print('')
+# print('total', 100+150+120)
+
+# for i in range(10):
+#   print(i)
+
+# for i in range(10):
+#   print(i , end=',')
+
+# print(len('python'))
+
+# print(len('祇園精舎の鐘の声、諸行無常の響きあり。沙羅双樹の花の色は、盛者必衰の理をあらわす。'))
+
+# print(print())
+
+# print('python',end='!')
+# print('happy')
+# print(end='!', 'python')  # error
+
+for i in range(3):
+    print('start')
+    print('stop')
+
+
+print()
+
+for i in range(3):
+    print('start')
+print('stop')
+
